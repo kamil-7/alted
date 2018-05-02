56193e102168d61a354441f356126e41d5b3b09f
fsdevio <moczydlowski.kamil@gmail.com>
2018-05-02 20:04:53 +0200

    fixed generating version number

--- utils/generate_version.py
+++ utils/generate_version.py
@@ -8,5 +8,8 @@ version = '{}+{}'.format(version, os.environ['CIRCLE_BUILD_NUM'])
 with open('.env.production', 'a') as file:
     file.write('\nVERSION=' + version)
 
+with open('.env', 'w') as file:
+    file.write('\nVERSION=' + version)
+
 with open('version', 'w') as file:
     file.write(version)
